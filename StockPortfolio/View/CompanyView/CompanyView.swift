ccf6dc361688dcc4e23bb0673a58965ea39af7a7
Ivan Ruiz Monjo <ivanruizmonjo@gmail.com>
2020-10-13 10:35:38 +0200

    added tags section

--- StockPortfolio/View/CompanyView/CompanyView.swift
+++ StockPortfolio/View/CompanyView/CompanyView.swift
@@ -12,24 +12,39 @@ struct CompanyView: View {
 
     let symbol: String
 
-    @StateObject var viewModel = CompanyViewModel()
-
-        var companyInfo: [(key: String, image: String, value: String)] {  [
-            ("Symbol", "textformat.abc", viewModel.company!.symbol),
-                ("Exchange", "dollarsign.square", viewModel.company!.exchange),
-                ("Industry", "wrench", viewModel.company!.industry),
-                ("CEO", "figure.wave", viewModel.company!.ceo),
-                ("Employees", "person.3", viewModel.company!.employees.description),
-                ("Country", "flag", viewModel.company!.country),
-            ]
-        }
-
+    @StateObject private var viewModel = CompanyViewModel()
+    var companyInfo: [(key: String, image: String, value: String)] {  [
+        ("Symbol", "textformat.abc", viewModel.company!.symbol),
+        ("Exchange", "dollarsign.square", viewModel.company!.exchange),
+        ("Industry", "wrench", viewModel.company!.industry),
+        ("CEO", "figure.wave", viewModel.company!.ceo),
+        ("Employees", "person.3", viewModel.company!.employees.description),
+        ("Country", "flag", viewModel.company!.country),
+    ]
+    }
+    let rows: [GridItem] =  [
+        GridItem(.flexible())
+    ]
     var body: some View {
         if let logoURL = viewModel.logoURL, let company = viewModel.company {
             List {
                 RemoteImage(url: logoURL)
                     .aspectRatio(contentMode: .fill)
                     .padding()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHGrid(rows: rows) {
+                        if let tags = viewModel.company?.tags {
+                            ForEach(tags, id: \.self) { tag in
+                                Text(tag)
+                                    .padding(8)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 16).fill(Color.Stock.gray)
+                                    ).font(.footnote)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
+                }.padding(.vertical)
                 Section(header: Text("About")) {
                     Text(company.description)
                         .padding(.bottom)
